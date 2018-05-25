153d14e94df52a1e85ee5cd1c4dda78cc3393d62
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-25 17:56:48 -0300

    utils/grep: list files stored in a bucket

--- utils/grep.go
+++ utils/grep.go
@@ -22,8 +22,45 @@
 
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
 
+// Grep identifies occurrences of a given string or pattern
+// on files stored in a S3 bucket
 func Grep(bucket string) {
-	fmt.Println("Called grep")
+	files := listObjects(bucket)
+	fmt.Println(files)
+}
+
+func listObjects(bucket string) (files []string) {
+	if len(bucket) < 2 {
+		fmt.Println("you must specify a bucket")
+		return
+	}
+
+	sess := session.Must(session.NewSession())
+
+	svc := s3.New(sess)
+
+	i := 0
+	err := svc.ListObjectsPages(&s3.ListObjectsInput{
+		Bucket: &bucket,
+	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
+		fmt.Println("Page,", i)
+		i++
+
+		for _, obj := range p.Contents {
+			files = append(files, string(*obj.Key))
+		}
+		return true
+	})
+
+	if err != nil {
+		fmt.Println("failed to list objects", err)
+		return
+	}
+	return
 }
