66181730a4b9c940c8b5eb0803cdbffdc401afdc
pedro.mendes <pedro.mendes@corp.globo.com>
2018-06-05 10:02:51 -0300

    cmd/test: add a file for test utilities

---
+++ utils/test.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"os"
+	"bytes"
+	"io"
+)
+
+// Captures the output by replacing the
+// standard stdout with a buffer and then restore it again.
+// Returns the buffer contents as String
+func captureOutput(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
