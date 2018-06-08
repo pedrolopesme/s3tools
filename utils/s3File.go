1289fbcc5e6349bec68eda097eb3086a3ce5fe1d
pedro.mendes <pedro.mendes@corp.globo.com>
2018-06-08 18:44:48 -0300

    utils/s3File: remove interface S3File

--- utils/s3File.go
+++ utils/s3File.go
@@ -7,25 +7,23 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-type S3File interface {
-	GetBucket() string
-	GetPath() string
-	GetBufferedContent() ([]byte, error)
-}
-
 type S3BufferedFile struct {
 	Bucket string
 	Path   string
 }
 
+// GetBucket returns file path
 func (f S3BufferedFile) GetBucket() (string) {
 	return f.Bucket
 }
 
+// GetPath returns file path
 func (f S3BufferedFile) GetPath() (string) {
 	return f.Path
 }
 
+// GetBufferedContent creates a S3 session and reads the content
+// of a file writing it into a buffer.
 func (f S3BufferedFile) GetBufferedContent() (buf []byte, err error) {
 	sess := session.Must(session.NewSession())
 	downloader := s3manager.NewDownloader(sess)
@@ -43,4 +41,14 @@ func (f S3BufferedFile) GetBufferedContent() (buf []byte, err error) {
 
 	buf = awsBuffer.Bytes()
 	return
+}
+
+// NewBufferedFile returns a concrete type of S3BufferedFile
+// properly initialized. Following recommendations found at
+// https://github.com/golang/go/wiki/CodeReviewComments#interfaces
+func NewBufferedFile(bucket string, path string) S3BufferedFile {
+	return S3BufferedFile{
+		Bucket: bucket,
+		Path:   path,
+	}
 }
