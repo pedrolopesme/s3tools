a0923136b1a7e9445aba80209a8cf6b7ebc286a3
pedro.mendes <pedro.mendes@corp.globo.com>
2018-06-15 18:10:28 -0300

    utils/list: add support to filter listings with a path prefix

--- utils/list.go
+++ utils/list.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 // ListObjects retrieves a list of files from a given
 // bucket and return a s3File list.
-func ListObjects(bucket string) (files []s3File) {
+func ListObjects(bucket string, path string) (files []s3File) {
 	if len(bucket) < 2 {
 		fmt.Println("you must specify a bucket")
 		return nil
@@ -16,16 +17,21 @@ func ListObjects(bucket string) (files []s3File) {
 
 	sess := session.Must(session.NewSession())
 	svc := s3.New(sess)
-	err := svc.ListObjectsPages(&s3.ListObjectsInput{
-		Bucket: &bucket,
-	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
+
+	listObjectsInput := &s3.ListObjectsInput{ Bucket: &bucket }
+	if path != "" {
+		listObjectsInput.Prefix = aws.String(path)
+	}
+
+	fnCallback := func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
 		for _, s3Object := range p.Contents {
 			file := NewBufferedFile(bucket, string(*s3Object.Key))
 			files = append(files, file)
 		}
 		return true
-	})
+	}
 
+	err := svc.ListObjectsPages(listObjectsInput, fnCallback)
 	if err != nil {
 		fmt.Println("failed to list objects", err)
 		return
