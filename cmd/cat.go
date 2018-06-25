637dcb74f9a701b04ab86ce5410beab8d7463f2c
pedro.mendes <pedro.mendes@corp.globo.com>
2018-06-25 09:41:59 -0300

    cmd/cat: add cat command placeholder

---
+++ cmd/cat.go
@@ -0,0 +1,58 @@
+//MIT License
+//
+//Copyright (c) 2018 Pedro Mendes
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// catCmd represents the cat command
+var catCmd = &cobra.Command{
+	Use:   "cat",
+	Short: "Concatenate and print files",
+	Long: `The cat utility reads files sequentially, writing them to the standard output.  
+The file operands are processed in command-line order. For example:
+
+	s3tools cat file1
+
+will print the contents of file1 to the standard output.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("cat called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(catCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// catCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// catCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
