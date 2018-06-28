108995dd1db342782f3755293d0d7d93d7c07734
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-06-27 21:13:02 -0300

    cmd/cat: add cat utility file

---
+++ utils/cat.go
@@ -0,0 +1,40 @@
+// MIT License
+//
+// Copyright (c) 2018 Pedro Mendes
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package utils
+
+import (
+	"fmt"
+)
+
+// Array of files found by filename expression parser
+type files []string
+
+// CatFiles will read all files matching their name to the user input
+// and print the content to the standard output
+func CatFiles(files []string) {
+	if len(files) ==0  {
+		fmt.Println("You must provide at least one file name")
+		return
+	}
+	fmt.Println("GrepFiles finished.")
+}
